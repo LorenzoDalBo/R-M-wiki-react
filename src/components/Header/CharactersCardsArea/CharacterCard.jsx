603af3ce3a8e7eaf1173c401d483e758ab4dd9d6
Lorenzo Trevizol Dal Bo <lorenzo.41522@alunosatc.edu.br>
2023-05-24 18:40:07 -0300

    saida

--- src/components/Header/CharactersCardsArea/CharacterCard.jsx
+++ src/components/Header/CharactersCardsArea/CharacterCard.jsx
@@ -1,8 +1,8 @@
 function CharacterCard({character}) {
     return(
-        <div className="flex flex-col border-solid border-4 border-blue-500 bg-slate-400 w-full p-2 text-center h-auto rounded-xl">
+        <div className="flex flex-col border-solid border-4 border-blue-600 bg-slate-400 w-full p-2 text-center h-auto rounded-xl">
             <div className="flex justify-center">
-                <img className="w-1/2 flex justify-center" src={character?.image}/>
+                <img className="w-1/2 border-2 border-solid border-blue-600 flex justify-center" src={character?.image}/>
             </div>
             <div className="text-start h-fit">
                 <h2 className="text-xl"><strong>{character?.name}</strong></h2>
